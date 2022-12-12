eecf5595243c736b1716627d13a3681a30b18e7d
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-12-12 12:59:48 +0000

    Correction

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,21 @@
-import ctypes
+#include <Python.h>
+#include <listobject.h>
 
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(l)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+void print_python_list_info(PyObject *p)
+{
+    // Get the size and allocated size of the Python list
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+    // Print the size and allocated size of the Python list
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+
+    // Iterate over the elements of the Python list and print their type
+    for (int i = 0; i < size; i++)
+    {
+        PyObject *element = PyList_GetItem(p, i);
+        printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
+    }
+}
+
